7d8dd5e5ca6daca53a1e3759d02df73b452c5129
SpaceBird <spacebird@pop-os.localdomain>
2021-10-22 21:29:59 +0200

    add image preview to website

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,69 +1,84 @@
-import pkg from './package'
+import pkg from "./package";
 
 export default {
-  mode: 'universal',
+  mode: "universal",
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description },
+      { hid: "og:type", name: "og:type", content: "website" },
+      {
+        hid: "og:image",
+        name: "og:image",
+        content: "/electro-story-desktop-and-mobile-new-version/preview.jpg"
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/electro-story-desktop-and-mobile-new-version/favicon.ico' },
-      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Rubik:300,300i,400,700&display=swap'},
-      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Karla:400,400i&display=swap'}
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/electro-story-desktop-and-mobile-new-version/favicon.ico"
+      },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Rubik:300,300i,400,700&display=swap"
+      },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Karla:400,400i&display=swap"
+      }
     ]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
 
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
 
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
+    "@nuxtjs/axios",
+    "@nuxtjs/pwa"
   ],
   /*
-  ** Axios module configuration
-  */
+   ** Axios module configuration
+   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
   },
 
   router: {
-    base: '/electro-story-desktop-and-mobile-new-version/'
-},
+    base: "/electro-story-desktop-and-mobile-new-version/"
+  },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
